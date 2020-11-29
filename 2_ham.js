fcce259e39d6242fd34048d9d86265fc83cd2ed8
ndtien95 <66723408+ndtien95@users.noreply.github.com>
2020-11-29 16:03:21 +0700

    commit branch

--- 2_ham.js
+++ 2_ham.js
@@ -1,13 +1,5 @@
 
-//Cách 1 
- /*function tong(a,b)
-{
-	return eval(a)+b;
-}
-var x =tong('5',5);
-console.log(x);*/
 
-//Cách 2: gọi hàm từ hàm khác
 
 function hello(){
 	console.log('Hello every body');

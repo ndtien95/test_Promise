a8f6837610c5eb31a3fabffcef0166b583dde16d
ndtien95 <66723408+ndtien95@users.noreply.github.com>
2021-02-21 22:07:26 +0700

    Empty commit

--- 2_ham.js
+++ 2_ham.js
@@ -1,14 +1,1 @@
-console.log("Hello master")
 
-function hello(){
-	console.log('Hello every body');
-}
-
-
-function callback(fn){
-	fn();
-}
-callback(hello);
-
-console.log("commit by master")
-console.log("this is branch PC 1")
